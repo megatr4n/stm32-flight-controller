df7a7b8fad39059575a464477f1d92ae6a54b191
tajmaha1 <d.cherniavskyi@ukma.edu.ua>
2026-04-09 23:20:53 +0300

    add modular I2C driver for STM32

---
+++ src/drivers/i2c_driver.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "stm32f4xx_hal.h"
+
+extern I2C_HandleTypeDef hi2c1;
+
+void I2C1_Init(void);
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+struct Motors {
+    float fl;
+    float fr;
+    float rl;
+    float rr;
+};
+
+Motors mix_motors(float gas, float roll, float pitch) {
+    Motors m;
+    
+}
